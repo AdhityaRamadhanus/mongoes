932d84792d2d6d59edf080c461d93a283df3164b
Adhitya Ramadhanus <adhitya.ramadhanus@gmail.com>
2016-12-03 16:41:36 +0700

    refactor gowatch to viper, restructure project, handle error better

---
+++ gowatch/elastic.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/AdhityaRamadhanus/mongoes"
+	"gopkg.in/mgo.v2/bson"
+	elastic "gopkg.in/olivere/elastic.v5"
+)
+
+func getMapping(es_options mongoes.ESOptions) ([]string, error) {
+	// Get elastic search mapping
+	client, err := elastic.NewClient(elastic.SetURL(es_options.ES_URI))
+	if err != nil {
+		return nil, err
+	}
+
+	rawMapping, err := client.GetMapping().Index(es_options.ES_index).Type(es_options.ES_type).Pretty(true).Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	jsonPath := es_options.ES_index + ".mappings." + es_options.ES_type + ".properties"
+	esMapping := mongoes.GetDeepObject(rawMapping, jsonPath)
+	selectedField := make([]string, 1)
+	for key, _ := range esMapping {
+		selectedField = append(selectedField, key)
+	}
+	return selectedField, nil
+}
+
+func processOplog(es_options mongoes.ESOptions, selectedField []string) chan<- Oplog {
+	oplogs := make(chan Oplog, 1000)
+	go func(es_options mongoes.ESOptions, selectedField []string) {
+		client, err := elastic.NewClient(elastic.SetURL(es_options.ES_URI))
+		if err != nil {
+			return
+		}
+		indexService := elastic.NewIndexService(client).Index(es_options.ES_index).Type(es_options.ES_type)
+		deleteService := elastic.NewDeleteService(client).Index(es_options.ES_index).Type(es_options.ES_type)
+
+		for p := range oplogs {
+			if p.Op == "i" || p.Op == "u" {
+				indexRequest := map[string]interface{}{}
+				for _, v := range selectedField {
+					if p.O[v] != nil {
+						indexRequest[v] = p.O[v]
+					}
+				}
+				stringId := p.O["_id"].(bson.ObjectId).Hex()
+				if _, err := indexService.Id(stringId).BodyJson(indexRequest).Do(context.Background()); err == nil {
+					fmt.Println("Successfully indexed")
+				}
+			} else if p.Op == "d" {
+				deleteRequestId := p.O["_id"].(bson.ObjectId).Hex()
+				fmt.Println(deleteRequestId)
+				if _, err := deleteService.Id(deleteRequestId).Do(context.Background()); err == nil {
+					fmt.Println("Successfully deleted")
+				}
+			}
+		}
+	}(es_options, selectedField)
+	return oplogs
+}
--- gowatch/main.go
+++ gowatch/main.go
@@ -1,98 +1,69 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"flag"
 	"fmt"
 	"github.com/AdhityaRamadhanus/mongoes"
+	"github.com/spf13/viper"
 	mongo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-	elastic "gopkg.in/olivere/elastic.v5"
 	"os"
 	"time"
 )
 
 func fatal(e error) {
 	fmt.Println(e)
+	fmt.Println("For More information see https://github.com/AdhityaRamadhanus/mongoes/blob/master/README.md")
 	flag.PrintDefaults()
 }
 
-func processOplog(client *elastic.Client, indexName, typeName string, selectedField []string, oplogs <-chan Oplog) {
-	indexService := elastic.NewIndexService(client).Index(indexName).Type(typeName)
-	deleteService := elastic.NewDeleteService(client).Index(indexName).Type(typeName)
+var (
+	es_options  mongoes.ESOptions
+	mgo_options mongoes.MgoOptions
+	configName  = flag.String("config", "", "config file")
+	pathConfig  = flag.String("path", ".", "config path")
+)
 
-	for p := range oplogs {
-		if p.Op == "i" || p.Op == "u" {
-			indexRequest := map[string]interface{}{}
-			for _, v := range selectedField {
-				if p.O[v] != nil {
-					indexRequest[v] = p.O[v]
-				}
-			}
-			stringId := p.O["_id"].(bson.ObjectId).Hex()
-			if _, err := indexService.Id(stringId).BodyJson(indexRequest).Do(context.Background()); err == nil {
-				fmt.Println("Successfully indexed")
-			}
-		} else if p.Op == "d" {
-			deleteRequestId := p.O["_id"].(bson.ObjectId).Hex()
-			fmt.Println(deleteRequestId)
-			if _, err := deleteService.Id(deleteRequestId).Do(context.Background()); err == nil {
-				fmt.Println("Successfully deleted")
-			}
-		}
+func init() {
+	flag.Parse()
+	if len(*configName) == 0 {
+		fatal(errors.New("Please provide config file and config path"))
+		os.Exit(1)
 	}
+	viper.SetConfigName(*configName)
+	viper.AddConfigPath(*pathConfig)
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		fatal(err)
+		os.Exit(1)
+	}
+	mgo_options.Mgo_dbname = viper.GetString("mongodb.database")
+	mgo_options.Mgo_collname = viper.GetString("mongodb.collection")
+	mgo_options.Mgo_URI = viper.GetString("mongodb.uri")
+
+	es_options.ES_index = viper.GetString("elasticsearch.index")
+	es_options.ES_type = viper.GetString("elasticsearch.type")
+	es_options.ES_URI = viper.GetString("elasticsearch.uri")
 }
 
 func main() {
-	var dbName = flag.String("db", "", "Mongodb DB Name")
-	var collName = flag.String("collection", "", "Mongodb Collection Name")
-	var dbUri = flag.String("dbUri", "localhost:27017", "Mongodb URI")
-	var indexName = flag.String("index", "", "ES Index Name")
-	var typeName = flag.String("type", "", "ES Type Name")
-	var esUri = flag.String("esUri", "http://localhost:9200", "Elasticsearch URI")
-
-	flag.Parse()
-
-	if len(*dbName) == 0 || len(*collName) == 0 {
-		fatal(errors.New("Please provide db and collection name"))
-		return
-	}
-
-	if len(*indexName) == 0 {
-		indexName = dbName
-	}
-
-	if len(*typeName) == 0 {
-		typeName = collName
-	}
-
 	// Set Tracer
 	tracer := mongoes.NewTracer(os.Stdout)
 
-	// Get elastic search mapping
-	tracer.Trace("Connecting to elasticsearch cluster")
-	client, err := elastic.NewClient(elastic.SetURL(*esUri))
+	// Get connected to Elasticsearch
+	tracer.Trace("Connecting to Elasticsearch cluster at", es_options.ES_URI)
+	selectedField, err := getMapping(es_options)
 	if err != nil {
 		fatal(err)
 		return
 	}
-
-	rawMapping, err := client.GetMapping().Index(*indexName).Type(*typeName).Pretty(true).Do(context.Background())
-	if err != nil {
-		fatal(err)
-		return
-	}
-	jsonPath := *indexName + ".mappings." + *typeName + ".properties"
-	esMapping := mongoes.GetDeepObject(rawMapping, jsonPath)
-	selectedField := make([]string, 1)
-	for key, _ := range esMapping {
-		selectedField = append(selectedField, key)
-	}
+	oplogs := processOplog(es_options, selectedField)
 
 	// Get connected to mongodb
-	tracer.Trace("Connecting to Mongodb at", *dbUri)
-	session, err := mongo.Dial(*dbUri)
+	tracer.Trace("Connecting to Mongodb at", mgo_options.Mgo_URI)
+	session, err := mongo.Dial(mgo_options.Mgo_URI)
 	if err != nil {
 		fatal(err)
 		return
@@ -104,10 +75,8 @@ func main() {
 	lastId <<= 32
 	lastId |= 1
 	var p Oplog
-	var nstring = *dbName + "." + *collName
+	var nstring = mgo_options.Mgo_dbname + "." + mgo_options.Mgo_collname
 	iter := collection.Find(bson.M{"ns": nstring, "ts": bson.M{"$gt": lastId}}).Tail(5 * time.Second)
-	oplogs := make(chan Oplog, 1000)
-	go processOplog(client, *indexName, *typeName, selectedField, oplogs)
 	for {
 		for iter.Next(&p) {
 			lastId = p.Ts
