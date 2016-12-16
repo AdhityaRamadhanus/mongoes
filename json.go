978f312d9a8fcab90e8fe7924eeb002a92e6751f
Adhitya Ramadhanus <adhitya.ramadhanus@gmail.com>
2016-12-17 00:08:49 +0700

    add readjsonfromfile func

--- json.go
+++ json.go
@@ -1,8 +1,22 @@
 package mongoes
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"strings"
 )
+
+func ReadJSONFromFile(path string, v *map[string]interface{}) error {
+	fileBytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(fileBytes, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 // GetDeepObject return object within JSON object
 func GetDeepObject(obj map[string]interface{}, path string) map[string]interface{} {
