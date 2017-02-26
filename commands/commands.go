b3123597042d604a9319209b36be6b17f7ac4090
Adhitya Ramadhanus <adhitya.ramadhanus@gmail.com>
2017-02-26 22:47:22 +0700

    refactor gondex to use urfave/cli

---
+++ commands/commands.go
@@ -0,0 +1,25 @@
+package commands
+
+import (
+	cli "github.com/urfave/cli"
+)
+
+var Commands = cli.Commands{
+	{
+		Name:   "index",
+		Usage:  "Index collection to elasticsearch",
+		Action: indexMongoToES,
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "config",
+				Value: "",
+				Usage: "Configuration file path",
+			},
+			cli.IntFlag{
+				Name:  "workers",
+				Value: 2,
+				Usage: "Number of workers to index the mongo collection",
+			},
+		},
+	},
+}
